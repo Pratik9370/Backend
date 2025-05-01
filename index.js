159a457375134ac3a0885e493647b151e5c45dad
Pratik Jadhav <pratikvj9370@gmail.com>
2025-05-01 12:53:03 +0530

    frontend on vercel with github

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ const path = require('path');
 
 connectToMongo()
 
-app.use(cors({
-    origin: 'https://glittery-arithmetic-b2cbe0.netlify.app/', // frontend URL
-    credentials: true,               // To Allow cookies to be sent
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://glittery-arithmetic-b2cbe0.netlify.app',
+    "https://i-notebook-frontend-9hua0vpr8-pratik-jadhavs-projects-70fe9421.vercel.app/"
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
   }));
+  
 app.use(express.json())
 app.use(cookieParser())
 
